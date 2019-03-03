65b26864814fdf08852b2dcc3f6c348096948b61
ysl <vladsugak@gmail.com>
2019-03-02 18:29:39 -0800

    Update api.py

--- api.py
+++ api.py
@@ -10,7 +10,9 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 
 database = 'hospitalDB.db'
-connection = sqlite3.connect(database)
+#connection = sqlite3.connect(database)
+
+app = Flask(__name__)
 
 #Checks if Patient has middle name, returns FirstName + MiddleName + LastName as Name
 def checkMiddleName(firstName, middleName, lastName):
@@ -22,106 +24,112 @@ def checkMiddleName(firstName, middleName, lastName):
 
 #Returns JSON of all patients
 def showAllPatients():
-    cursor = connection.cursor()
-    cursor.execute(
-        'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE Patient.DoctorID = Doctor.ID;'
-        )
-    rows = cursor.fetchall()
-    
-    patientDict = dict()
-    count = 0
-    for row in rows:
-        name = checkMiddleName(row[0], row[1], row[2])
-        patientDict[count] = {'Name': name, 'Gender': row[3], 'DOB': row[4], 'Address': row[5], 'Phone': row[6], 'InsuranceNumber': row[7], 'PrimaryDoctor': row[8]}
-        count += 1
-    patientJson = json.dumps(patientDict)
+    with sqlite3.connect(database) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE Patient.DoctorID = Doctor.ID;'
+            )
+        rows = cursor.fetchall()
+        
+        patientDict = dict()
+        count = 0
+        for row in rows:
+            name = checkMiddleName(row[0], row[1], row[2])
+            patientDict[count] = {'Name': name, 'Gender': row[3], 'DOB': row[4], 'Address': row[5], 'Phone': row[6], 'InsuranceNumber': row[7], 'PrimaryDoctor': row[8]}
+            count += 1
+        patientJson = json.dumps(patientDict)
+        connection.commit()
     return patientJson
 
 #find Patient in DB by insurance number or name. If no input = return all patients
+@app.route('/findPatient/<firstName>/<middleName>/<lastName>/<insuranceNum>')
 def findPatient(firstName, middleName, lastName, insuranceNum):
-    cursor = connection.cursor()
-    #if all parameters are empty, return all patients in DB
-    if firstName == None and lastName == None and middleName == None and insuranceNum == None:
-        return showAllPatients()
-    #Search by insuranceNum if its present
-    elif insuranceNum != None:
-        cursor.execute(
-        'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE InsuranceNumber = ? AND Patient.DoctorID = Doctor.ID;', (int(insuranceNum), )
-        )
-    #if insuranceNum is not present, search other params
-    elif insuranceNum == None:
-        if middleName == None or middleName == 'None':
+    if firstName == 'None': firstName = None
+    if lastName == 'None': lastName = None
+    if middleName == 'None': middleName = None
+    if insuranceNum == 'None': insuranceNum = None
+    
+    with sqlite3.connect(database) as connection:
+        cursor = connection.cursor()
+        #if all parameters are empty, return all patients in DB
+        if firstName == None and lastName == None and middleName == None and insuranceNum == None:
+            return showAllPatients()
+        #Search by insuranceNum if its present
+        elif insuranceNum != None:
             cursor.execute(
-            'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE FirstName = ? AND LastName = ? AND Patient.DoctorID = Doctor.ID;', (str(firstName), str(lastName))
+            'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE InsuranceNumber = ? AND Patient.DoctorID = Doctor.ID;', (int(insuranceNum), )
             )
-        else:
-            cursor.execute(
-            'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE FirstName = ? AND MiddleName = ? AND LastName = ? AND Patient.DoctorID = Doctor.ID;', (str(firstName), str(middleName), str(lastName))
-            )     
+        #if insuranceNum is not present, search other params
+        elif insuranceNum == None:
+            if middleName == None or middleName == 'None':
+                cursor.execute(
+                'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE FirstName = ? AND LastName = ? AND Patient.DoctorID = Doctor.ID;', (str(firstName), str(lastName))
+                )
+            else:
+                cursor.execute(
+                'SELECT Patient.FirstName, Patient.MiddleName, Patient.LastName, Gender, DateOfBirth, Patient.Address, Patient.Phone, InsuranceNumber, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM PATIENT, DOCTOR WHERE FirstName = ? AND MiddleName = ? AND LastName = ? AND Patient.DoctorID = Doctor.ID;', (str(firstName), str(middleName), str(lastName))
+                )     
 
-    rows = cursor.fetchall()
-    patientDict = dict()
-    count = 0
-    for row in rows:
-        name = checkMiddleName(row[0], row[1], row[2])
-        patientDict[count] = {'Name': name, 'Gender': row[3], 'DOB': row[4], 'Address': row[5], 'Phone': row[6], 'InsuranceNumber': row[7], 'PrimaryDoctor': row[8]}
-        count += 1
-    patientJson = json.dumps(patientDict)
+        rows = cursor.fetchall()
+        patientDict = dict()
+        count = 0
+        for row in rows:
+            name = checkMiddleName(row[0], row[1], row[2])
+            patientDict[count] = {'Name': name, 'Gender': row[3], 'DOB': row[4], 'Address': row[5], 'Phone': row[6], 'InsuranceNumber': row[7], 'PrimaryDoctor': row[8]}
+            count += 1
+        patientJson = json.dumps(patientDict)
+        connection.commit()
     return patientJson
 
-#returns info about patient's diagnoses
-def getDiagnosis(insuranceNum):
-    cursor = connection.cursor()
-    cursor.execute('SELECT Diagnosis.ConditionName, PATIENTS_DIAGNOSES.DiagnosisDate, PATIENTS_DIAGNOSES.Remarks FROM PATIENT, PATIENTS_DIAGNOSES, DIAGNOSIS WHERE Patient.insurancenumber = ? AND Diagnosis.id = Patients_Diagnoses.DiagnosisID AND PAtients_Diagnoses.PatientID = Patient.ID;', (int(insuranceNum), )
-    )
-    rows = cursor.fetchall()
-    diagnosisDict = dict()
-    for row in rows:
-        diagnosisDict['Diagnoses'] = {row[0]: {'Diagnosed on': row[1], 'Remarks': row[2]}}
-    return diagnosisDict
+# #returns info about patient's diagnoses
+# def getDiagnosis(insuranceNum):
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT Diagnosis.ConditionName, PATIENTS_DIAGNOSES.DiagnosisDate, PATIENTS_DIAGNOSES.Remarks FROM PATIENT, PATIENTS_DIAGNOSES, DIAGNOSIS WHERE Patient.insurancenumber = ? AND Diagnosis.id = Patients_Diagnoses.DiagnosisID AND PAtients_Diagnoses.PatientID = Patient.ID;', (int(insuranceNum), )
+#     )
+#     rows = cursor.fetchall()
+#     diagnosisDict = dict()
+#     for row in rows:
+#         diagnosisDict['Diagnoses'] = {row[0]: {'Diagnosed on': row[1], 'Remarks': row[2]}}
+#     return diagnosisDict
 
-#returns prescribed medication
-def getPrescription(insuranceNum):
-    cursor = connection.cursor()
-    cursor.execute('SELECT Medication.name, Prescription.PrescribedAmount, Medication.MaxDosage, Prescription.Refill FROM Patient, Prescription, Medication WHERE Patient.insurancenumber = ? AND Medication.id = Prescription.medicationID AND Patient.id = Prescription.patientID;', (int(insuranceNum), ))
-    rows = cursor.fetchall()
-    prescriptionsDict = dict()
-    for row in rows:
-        if int(row[3]) == 1:
-            refill = 'Yes'
-        else:
-            refill = 'No'
-        prescriptionsDict['PrescribedMedication'] = {row[0]: {'PrescribedAmount': row[1], 'MaxDosage': row[2], 'Refill': refill}}
-    return prescriptionsDict
+# #returns prescribed medication
+# def getPrescription(insuranceNum):
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT Medication.name, Prescription.PrescribedAmount, Medication.MaxDosage, Prescription.Refill FROM Patient, Prescription, Medication WHERE Patient.insurancenumber = ? AND Medication.id = Prescription.medicationID AND Patient.id = Prescription.patientID;', (int(insuranceNum), ))
+#     rows = cursor.fetchall()
+#     prescriptionsDict = dict()
+#     for row in rows:
+#         if int(row[3]) == 1:
+#             refill = 'Yes'
+#         else:
+#             refill = 'No'
+#         prescriptionsDict['PrescribedMedication'] = {row[0]: {'PrescribedAmount': row[1], 'MaxDosage': row[2], 'Refill': refill}}
+#     return prescriptionsDict
 
-#returns appointments
-def getAppointments(insuranceNum):
-    cursor = connection.cursor()
-    cursor.execute('SELECT Appointment.StartTime, Duration, Description, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM Patient, Appointment, Doctor, APPOINTMENT_DOCTORS WHERE Patient.id = Appointment.PatientID AND Appointment.ID = APPOINTMENT_DOCTORS.AppointmentID AND Doctor.id = Appointment_Doctors.doctorID AND Patient.insurancenumber = ?;', (int(insuranceNum), ))
-    rows = cursor.fetchall()
-    appointmentDict = dict()
-    for row in rows:
-        appointmentDict['Appointments'] = {row[0]: {'Duration': row[1], 'Description': row[2], 'Doctor': row[3]}}
-    return appointmentDict
+# #returns appointments
+# def getAppointments(insuranceNum):
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT Appointment.StartTime, Duration, Description, (Doctor.FirstName || " " || Doctor.LastName) as Doctor FROM Patient, Appointment, Doctor, APPOINTMENT_DOCTORS WHERE Patient.id = Appointment.PatientID AND Appointment.ID = APPOINTMENT_DOCTORS.AppointmentID AND Doctor.id = Appointment_Doctors.doctorID AND Patient.insurancenumber = ?;', (int(insuranceNum), ))
+#     rows = cursor.fetchall()
+#     appointmentDict = dict()
+#     for row in rows:
+#         appointmentDict['Appointments'] = {row[0]: {'Duration': row[1], 'Description': row[2], 'Doctor': row[3]}}
+#     return appointmentDict
 
-#returns all data associated with patient
-def getPatientInfo(insuranceNum):
-    diagnoses = getDiagnosis(insuranceNum)
-    prescription = getPrescription(insuranceNum)
-    appointments = getAppointments(insuranceNum)
-    patientInfoDict = {**diagnoses, **prescription, **appointments}
-    patientInfoJson = json.dumps(patientInfoDict)
-    return patientInfoJson
-
-#print(getPatientInfo(111000111))
-print(showAllPatients())
+# #returns all data associated with patient
+# def getPatientInfo(insuranceNum):
+#     diagnoses = getDiagnosis(insuranceNum)
+#     prescription = getPrescription(insuranceNum)
+#     appointments = getAppointments(insuranceNum)
+#     patientInfoDict = {**diagnoses, **prescription, **appointments}
+#     patientInfoJson = json.dumps(patientInfoDict)
+#     return patientInfoJson
 
 #Initialization of Flask's endpoints
-# application = Flask(__name__)
-#application.add_url_rule("/", "index", index)
-#application.add_url_rule("/game/start/buyperiod", "startBuyPeriod", startBuyPeriod, methods = ["POST"])
-#application.add_url_rule("/game/updatecoins", "updateCoins", updateCoins, methods = ["POST"])
+# app = Flask(__name__)
+#application.add_url_rule("/findPatient", "findPatient", startBuyPeriod, methods = ["POST"])
+#application.add_url_rule("/getPatientInfo", "updateCoins", updateCoins, methods = ["POST"])
     
-# if __name__ == "__main__":
-#     application.debug = True
-#     application.run()
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
